0fd20f20f2f5d7b44997055f1d7618c0f4206744
Moses Gomez <pacgames1405@gmail.com>
2018-01-03 16:35:11 -0700

    Taco
    taco

---
+++ RPG/Assets/Killfeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killfeed : MonoBehaviour {
+
+	[SerializeField]
+	GameObject killfeedItemPrfb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameManager.instance.onPlayerKilledCallback += OnKill;
+	}
+
+	public void OnKill(string player, string source)
+	{
+		GameObject go = (GameObject)Instantiate (killfeedItemPrfb, this.transform);
+		go.GetComponent<KillfeedItem>().Setup(player, source);
+		go.transform.SetAsFirstSibling ();
+		Destroy (go, 4f);
+	}
+}
---
+++ RPG/Assets/KillfeedItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class KillfeedItem : MonoBehaviour {
+
+	[SerializeField]
+	Text text;
+
+	public void Setup ( string player, string source)
+	{
+		text.text = "<b>" + source + "</b>" + " killed " + "<color=red>" + "<b>" + player + "</Color>" + "</b>";
+	}
+}
